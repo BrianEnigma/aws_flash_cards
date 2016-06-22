39c874df39d2e4790273f74bdca953e0b3798da7
Brian Enigma <brian@netninja.com>
2016-06-21 23:11:10 -0700

    Reverse + other joker

--- blank.rb
+++ blank.rb
@@ -17,19 +17,18 @@ SHADOW_COLOR_MAP = {
     "black" => "gray70",
 }
 
-def generate(bgcolor, color, filename)
-    card_value = "Joker"
-    suit_color = color
-    shadow_color = SHADOW_COLOR_MAP[color]
-    i = Magick::Image.new(WIDTH, HEIGHT) {self.background_color = "white"}
+def generate(bordercolor, textcolor, fieldcolor, text_string, filename)
+    suit_color = textcolor
+    shadow_color = SHADOW_COLOR_MAP[textcolor]
+    i = Magick::Image.new(WIDTH, HEIGHT) {self.background_color = fieldcolor}
     
     # Draw border
     border = Magick::Draw.new
-    border.fill = bgcolor
+    border.fill = bordercolor
     border.rectangle(0, 0, WIDTH, HEIGHT)
     border.draw(i)
     border = Magick::Draw.new
-    border.fill = 'white'
+    border.fill = fieldcolor
     border.roundrectangle(SAFE_OFFSET_X + COLOR_BORDER_WIDTH, SAFE_OFFSET_Y + COLOR_BORDER_WIDTH, SAFE_OFFSET_X + SAFE_WIDTH - COLOR_BORDER_WIDTH, SAFE_OFFSET_Y + SAFE_HEIGHT - COLOR_BORDER_WIDTH, 10, 10)
     border.draw(i)
     
@@ -44,27 +43,30 @@ def generate(bgcolor, color, filename)
     shadow_offset = 3
     [0, 180].each { |rotation|
         i.rotate!(rotation)
-        text.annotate(i, SAFE_WIDTH, SAFE_HEIGHT, SAFE_OFFSET_X + COLOR_BORDER_WIDTH + text_offset_x + shadow_offset, SAFE_OFFSET_Y + COLOR_BORDER_WIDTH + text_offset_y + shadow_offset, card_value) {
+        text.annotate(i, SAFE_WIDTH, SAFE_HEIGHT, SAFE_OFFSET_X + COLOR_BORDER_WIDTH + text_offset_x + shadow_offset, SAFE_OFFSET_Y + COLOR_BORDER_WIDTH + text_offset_y + shadow_offset, text_string) {
             self.fill = shadow_color 
         }
-        text.annotate(i, SAFE_WIDTH, SAFE_HEIGHT, SAFE_OFFSET_X + COLOR_BORDER_WIDTH + text_offset_x, SAFE_OFFSET_Y + COLOR_BORDER_WIDTH + text_offset_y, card_value) { 
+        text.annotate(i, SAFE_WIDTH, SAFE_HEIGHT, SAFE_OFFSET_X + COLOR_BORDER_WIDTH + text_offset_x, SAFE_OFFSET_Y + COLOR_BORDER_WIDTH + text_offset_y, text_string) { 
             self.fill = suit_color 
         }
     }
     
-    #AWS Flash Cards footer
-    text = Magick::Draw.new
-    text.font = 'Helvetica-Narrow'
-    text.pointsize = 18
-    text.gravity = Magick::NorthWestGravity
-    aws_flash_cards = "AWS Flash Cards\nv#{VERSION}, #{Time.new.strftime('%Y-%m-%d')}, http://nja.me/awscards\nProduced under the Creative Commons\nAttribution-ShareAlike 4.0 International License"
-    text.annotate(i, SAFE_WIDTH, SAFE_HEIGHT, SAFE_OFFSET_X + COLOR_BORDER_WIDTH + 15, SAFE_OFFSET_Y + SAFE_HEIGHT - COLOR_BORDER_WIDTH - 75, aws_flash_cards) {
-        self.fill = 'gray70' 
-    }
+    if (!text_string.empty?)
+        #AWS Flash Cards footer
+        text = Magick::Draw.new
+        text.font = 'Helvetica-Narrow'
+        text.pointsize = 18
+        text.gravity = Magick::NorthWestGravity
+        aws_flash_cards = "AWS Flash Cards\nv#{VERSION}, #{Time.new.strftime('%Y-%m-%d')}, http://nja.me/awscards\nProduced under the Creative Commons\nAttribution-ShareAlike 4.0 International License"
+        text.annotate(i, SAFE_WIDTH, SAFE_HEIGHT, SAFE_OFFSET_X + COLOR_BORDER_WIDTH + 15, SAFE_OFFSET_Y + SAFE_HEIGHT - COLOR_BORDER_WIDTH - 75, aws_flash_cards) {
+            self.fill = 'gray70' 
+        }
+    end
     
     i.write("output/#{filename}")
 end
 
-generate("#f8ae43", "black", "blank1.png")
-generate("#444444", "red", "blank2.png")
+generate("#f8ae43", "black", "white", "Joker", "blank1.png")
+generate("#444444", "red", "white", "Joker", "blank2.png")
+generate("white", "red", "#444444", "", "reverse.png")
 
